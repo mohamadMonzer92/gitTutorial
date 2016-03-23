9d5b76c6a3dca2d49b902929b80324b8ae8871e9
Mohamad Monzer <mohamad.monzer@outlook.com>
2016-03-23 22:48:53 +0200

    commit gitTutorial .txt and .js

--- gitTutorial.js
+++ gitTutorial.js
@@ -25,15 +25,30 @@ var gitTutorialModule{
 		$git add *.txt 			// Add all txt files in the project
 	}
 
-	var removeFileFromStagingArea = {
-		$git rm --cached fileName
-	}
-
 	var commit = {
 		$git commit -m "commit message" // use present tense
 	}
 
+	// get History list
 	var gitTimeLineHistory = {
 		$git log
+	}
+
+	// show unstaged differences since last commit
+	var showUnstagedDifferences = {
+		$git diff 			// get differences
+		$git diff --staged 	// get staged differences
+	}
+
+	// unstage Tip
+	// those commands does not remove modifications!! they just unstage files
+	var removeFileFromStagingArea = {
+		$git rm --cached fileName	// not from the tutorial
+		$git reset HEAD fileName	// unstage, where the HEAD refers to the last commit on the current brach
+	}
+
+	// to checkout a file and remove all changes since last commit
+	var removeChanges = {
+		$git checkout -- fileName
 	}
 }();
